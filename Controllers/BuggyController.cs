a0ff920915012bbf5575cfa0dec5c95239b3da34
Josue Garcia <yoshuah809@gmail.com>
2022-02-23 12:24:37 -0600

    added middleware for error handling and buggy controller

---
+++ Controllers/BuggyController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("not-found")]
+        public ActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult GetBadRequest()
+        {
+            return NotFound(new ProblemDetails { Title="This is a bad request"});
+        }
+
+        [HttpGet("unauthorized")]
+        public ActionResult GetUnauthorized()
+        {
+            return Unauthorized();
+        }
+
+        [HttpGet("validation-error")]
+        public ActionResult GetValidationError()
+        {
+            ModelState.AddModelError("Problem1", "This is the first error");
+            ModelState.AddModelError("Problem2", "This is the second error");
+            return ValidationProblem();
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult GetServerError()
+        {
+            throw new Exception("This is a server error");
+           
+        }
+    }
+}
+
--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -44,15 +45,18 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             app.UseCors(opt =>
             {
                 //opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "http://192.168.0.125:3000/", "*","*");
                 opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("*");
             });
 
+            app.UseMiddleware<ExceptionMiddleware>();
+            
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+               
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
